8b831cf6d248f3f3bb0537db799e0583ac8f963f
Lluis Suros <lluissuros@gmail.com>
2019-12-16 01:04:22 +0100

    confessions form

--- src/pages/CreateComments.js
+++ src/pages/CreateComments.js
@@ -1,20 +1,46 @@
 import React, { useState } from "react";
 import { Link, Redirect } from "react-router-dom";
 import AuthForm from "../components/AuthForm";
-import { Card } from "../components/StyledComponents";
+import {
+  Card,
+  Logo,
+  Form,
+  Input,
+  Button,
+  Error
+} from "../components/StyledComponents";
 import { logout } from "../utils/AuthHelperMethods";
 
 function CreateComments({ history }) {
+  const [confession, setConfession] = useState("");
+
   const handleLogout = () => {
     logout();
     history.push(`/`);
+  };
+
+  const handleSubmit = e => {
+    console.log(e);
+    e.preventDefault();
+    console.log("handleSubmit", confession);
+    setConfession("");
   };
 
   return (
     <div>
       <button onClick={() => handleLogout()}>logout</button>
       <Card>
-        <div>Testing database</div>
+        <div>Testing database: post something, and get it from db</div>
+        <form>
+          <textarea
+            placeholder="your text"
+            value={confession}
+            onChange={e => {
+              setConfession(e.target.value);
+            }}
+          ></textarea>
+          <button onClick={handleSubmit}>submit</button>
+        </form>
       </Card>
     </div>
   );
